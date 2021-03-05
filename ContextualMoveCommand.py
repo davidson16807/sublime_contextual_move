ecf5d670f34b93efa67e8a6ea813216188cd17c0
Carl Davidson <davids14@illinoisalumni.org>
2021-03-05 01:35:09 -0700

    ease of use overhaul

--- ContextualMoveCommand.py
+++ ContextualMoveCommand.py
@@ -1,61 +1,45 @@
 import sublime, sublime_plugin, datetime
 
-class ToggleMoveContextCommand(sublime_plugin.WindowCommand):
-	def run(self, value = None):
-		settings = self.window.settings();
-		context = settings.get('move_context')
-		if value:
-			if context != value:
-				context = value
-			else:
-				context = 'default'
-			
-			settings.set('move_context', context)
-			settings.set('move_context_rep', 1)
-		# print(context)
-		sublime.status_message('Context: ' + context.upper())
-
 class SetMoveContextCommand(sublime_plugin.WindowCommand):
 	def run(self, value, command=None, args={}):
 		settings = self.window.settings();
 		context = settings.get('move_context')
 		settings.set('move_context', value)
-		settings.set('move_context_rep', 1)
 		if command:	self.window.run_command(command, args)
 		# print(context)
 		sublime.status_message('Context: ' + context.upper())
-
-class SetRepContextCommand(sublime_plugin.WindowCommand):
-	def run(self, value):
-		settings = self.window.settings();
-		context = settings.get('move_context_rep')
-		settings.set('move_context_rep', value)
-		# sublime.status_message('Context: ' + context.upper())
 
 class DoOnceMoveContextCommand(sublime_plugin.WindowCommand):
 	def run(self, value):
 		settings = self.window.settings();
 		settings.set('move_context', value)
-		settings.set('move_context_do_once', True)
-		settings.set('move_context_rep', 1)
+		settings.set('move_context_do_once', value)
+
+class ExtremumMoveContextCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		settings = self.window.settings();
+		settings.set('move_context_extremum', True)
 
 class ContextualMoveCommand(sublime_plugin.WindowCommand):
 	def run(self, **commands):
 		settings = self.window.settings();
 		context = settings.get('move_context');
 		do_once = settings.get('move_context_do_once')
-		rep = settings.get('move_context_rep');
-		
-		context = context if context in commands else 'default'
-		rep = rep if rep else 1
+		extremum = settings.get('move_context_extremum')
+
+		context = context if context else 'default'
+		if extremum:
+			context = context+'_extremum' if context+'_extremum' in commands else 'default_extremum'
+		else:
+			context = context if context in commands else 'default'
+		print(context)
 		
 		if context in commands:
-			for i in range(0,rep):
-				command = commands[context]
-				args = command['args'] if 'args' in command else {}
-				self.window.run_command(command['command'], args)
-				print(command)
+			command = commands[context]
+			args = command['args'] if 'args' in command else {}
+			self.window.run_command(command['command'], args)
+			print(command)
 
-		if do_once:
-			settings.set('move_context_do_once', False)
-			settings.set('move_context', 'default')
+		if extremum:
+			settings.set('move_context_extremum', False)
+			# settings.set('move_context', 'default')
