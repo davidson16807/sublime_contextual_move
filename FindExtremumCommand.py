ccce32a3927380dce358d09f4b0bff2c1c68e97c
Carl Davidson <davids14@illinoisalumni.org>
2021-03-06 21:31:52 -0700

    fixing find_extremum

--- FindExtremumCommand.py
+++ FindExtremumCommand.py
@@ -6,5 +6,5 @@ import sublime, sublime_plugin, re
 
 class FindExtremumCommand(sublime_plugin.WindowCommand):
     def run(self, forward):
-        self.window.run_command("move_to", {"to": "eof" if forward else 'bof', "extend": False})
-        self.window.run_command('find_next' if not forward else 'find_prev')
+    	for i in range(0, 1000):
+    		self.window.run_command('find_next' if forward else 'find_prev')
