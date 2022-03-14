3ba1ecc1cee597c0cee7598b75c833ef21aa597b
Carl Davidson <davids14@illinoisalumni.org>
2022-03-14 10:08:02 -0600

    altering tab switch behavior so that selected tabs remain visible

--- MoveSheetSelection.py
+++ MoveSheetSelection.py
@@ -1,19 +1,22 @@
 
-import sublime, sublime_plugin, re
+import sublime_plugin
 
-class ExpandSheetSelectionCommand(sublime_plugin.WindowCommand):
-    def run(self, forward=True):
+class MoveSheetSelectionCommand(sublime_plugin.WindowCommand):
+    def run(self, forward=True, extend=False):
         offset = 1 if forward else -1
         active_sheet = self.window.active_sheet()
         all_sheets = self.window.sheets()
         new_sheet_id = (all_sheets.index(active_sheet) + offset) % len(all_sheets)
         new_sheet = all_sheets[new_sheet_id]
         selected_sheets = self.window.selected_sheets()
-        if new_sheet not in selected_sheets:
+        if not extend: 
+            pass
+        elif new_sheet not in selected_sheets:
             selected_sheets.append(new_sheet)
             self.window.select_sheets(selected_sheets)
-            self.window.focus_sheet(new_sheet)
         else:
             selected_sheets.remove(active_sheet)
             self.window.select_sheets(selected_sheets)
-            self.window.focus_sheet(new_sheet)
+        self.window.focus_sheet(new_sheet)
+
+
